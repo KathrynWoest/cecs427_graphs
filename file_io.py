59ed75a1a5865d1ad0d285d7da784e938d6eb3b8
KathrynWoest <kathryn.woest@gmail.com>
2026-02-02 16:40:19 -0800

    Add file output and error handling

--- file_io.py
+++ file_io.py
@@ -1,20 +1,39 @@
 import sys
 import networkx as nx
 
-# will temporarily use this as the main file for testing purposes
-
 def parse_graph(file_name):
     """Takes the input file and parses it into a NetworkX graph that can be analyzed
-    Input: file name of the submitted graph
+    Input: .gml file name of the submitted graph
     Output: NetworkX graph of the submitted graph from the file"""
+    
+    try:
+        submitted_graph = nx.read_gml(file_name)
+        return submitted_graph
+    
+    except Exception as e:
+        print("Program quit due to an error in reading and parsing the graph from the provided .gml file.")
+        print("Provided error:", e)
+        sys.exit(1)
 
-    # okay only keep this if there's error handling i can add, bc otherwise kind of a useless function lol
-    submitted_graph = nx.read_gml(file_name)
-    return submitted_graph
+def save_graph(graph, file_name):
+    """Takes a name for an output file, creates the file, and saves the graph and any analysis into it
+    Inputs: the graph itself, .gml file name for saving the graph
+    Output: none"""
+
+    try:
+        nx.write_gml(graph, file_name)
+
+    except Exception as e:
+        print("Program quit due to an error in creating the save file and saving the graph.")
+        print("Provided error:", e)
+        sys.exit(1)
+
 
 def main():
     args = sys.argv
     file_path = args[1]
     og_graph = parse_graph(file_path)
+    print(og_graph.nodes)
+    save_graph(og_graph, "temp_save.gml")
 
 main()
