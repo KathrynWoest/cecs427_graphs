98147a5a803a9806edd430191db2fe08c4fe829c
KathrynWoest <kathryn.woest@gmail.com>
2026-02-03 18:00:19 -0800

    Add generator and p calculator

--- generator.py
+++ generator.py
@@ -1,18 +1,29 @@
 import networkx as nx
 import sys
+import math
+
+def generation(n, c):
+    """Function that generates an Erdos Renyi graph given n nodes and c constant for probability that an edge exists between two nodes
+    Inputs: strings n and c, representing the number of nodes and a constant to calculate probability of edges respectively
+    Output: a NetworkX-structured Erdos Renyi graph"""
+
+    try:
+        node_count = int(n)
+        edge_const = int(c)
+        edge_prob = edge_const * (math.log(node_count) / node_count)
+        generated_graph = nx.erdos_renyi_graph(node_count, edge_prob)
+
+    except ValueError as ve:
+        print("Program terminated because graph generation input(s) 'n' and/or 'c' are not integers. Error message:", ve)
+        sys.exit(1)
+
+    except Exception as e:
+        print("Program terminated because there was an error with NetworkX generating the graph or calculating p. Error message:", e)
+        sys.exit(1)
+    
+    return generated_graph
 
 def main():
-
-    # if allowed to, can use nx.erdos_renyi_graph(n, c) to generate the graph
-    # if not allowed to:
-    #   create a generation file and loop through n nodes, manually writing each line to the file for the nodes
-    #   then use a for loop to iterate through every possible edge in the graph
-    #   the loop will randomly generate a number and compare it to c to determine if the edge exists
-    #   if it does, manually write the edge lines into the file
-    #   then read the file in with nx.parse_gml(file) to set it up as a networkx graph for analysis
-    #   finally, delete the generation file (if that's possible?), as it won't be needed again
-
-
-    pass
+    generation(6, 4)
 
 main()
