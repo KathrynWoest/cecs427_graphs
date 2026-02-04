f3a7653ebfdb5b2a05a9795d1e3b5dd5526d43f9
gracekflores <168140240+gracekflores@users.noreply.github.com>
2026-02-03 19:56:44 -0800

    added variables for plot information
    I added variables to store data that I will need to create a vizualization of the graph according the the instructions. The function returns the graph, the isolated nodes, and the edges that will be highlighted.

--- analyze.py
+++ analyze.py
@@ -25,16 +25,36 @@ def analyze(graph):
     print("Contains cycle?:", result)
 
     # Detecting isolated nodes
-    isolated = list(nx.isolates(G))
-    print("Isolated nodes:", isolated)
+    isolated_nodes = list(nx.isolates(G))
+    print("Isolated nodes:", isolated_nodes)
 
     # Computing density
     density = nx.density(G)
     print("Graph density:", density)
 
-    # Average shortest path length
+    # Computing average shortest path length
     if nx.is_connected(G):
         avg_path_len = nx.average_shortest_path_length(G)
         print("Average shortest path length:", avg_path_len)
     else:
         print("Graph is not connected; cannot compute average shortest path length.")
+
+    ## Data for plotting ##
+    # Computing BFS shortest paths from each root
+    bfs_roots = [next(iter(c)) for c in components]
+
+    shortest_paths = []
+    for root in bfs_roots:
+        lengths, paths = nx.single_source_dijkstra(G, root)  # works for weighted or unweighted
+        shortest_paths.append(paths)
+
+    # Marking edges that belong to any shortest path
+    highlight_edges = set()
+    for paths in shortest_paths:
+        for target, path in paths.items():
+            if len(path) > 1:
+                edges = list(zip(path[:-1], path[1:]))
+                highlight_edges.update(edges)
+
+
+    return G, isolated_nodes, highlight_edges
