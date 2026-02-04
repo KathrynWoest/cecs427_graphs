7ea24db0addc20587c21f57fe0f728291e5289b8
KathrynWoest <kathryn.woest@gmail.com>
2026-02-04 09:13:11 -0800

    Commented code and prepped for merging with my other branches

--- generator.py
+++ generator.py
@@ -5,11 +5,13 @@ import math
 def generation(n, c):
     """Function that generates an Erdos Renyi graph given n nodes and c constant for probability that an edge exists between two nodes
     Inputs: strings n and c, representing the number of nodes and a constant to calculate probability of edges respectively
-    Output: a NetworkX-structured Erdos Renyi graph"""
+    Output: a NetworkX-structured Erdos-Renyi graph"""
 
     try:
         node_count = int(n)
         edge_const = int(c)
+
+        # calculate probability of edges occuring with the given equation, then generate the graph
         edge_prob = edge_const * (math.log(node_count) / node_count)
         generated_graph = nx.erdos_renyi_graph(node_count, edge_prob)
 
@@ -22,8 +24,3 @@ def generation(n, c):
         sys.exit(1)
     
     return generated_graph
-
-def main():
-    generation(6, 4)
-
-main()
