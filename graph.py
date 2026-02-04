f1e9f9789526673af49d0267527fc3a4be55bfb1
KathrynWoest <kathryn.woest@gmail.com>
2026-02-04 09:21:33 -0800

    Commented code and prepped files for merging with my other branches

--- graph.py
+++ graph.py
@@ -1,31 +1,38 @@
 import sys
 
 def main():
+    # get arguments from command line and initialize BFS node list, the iterator to traverse arguments, and the end of the argument list
     args = sys.argv
     multi_BFS_nodes = []
     iterator = 1
     end = len(args)
 
+    # if there are less than 3 arguments, then not possible to do anything. terminate program.
     if end < 3:
         raise Exception(f"Program was terminated because there are not enough arguments to upload or generate a graph. Minimum required arguments: 2. Arguments provided: {end}.")
     
     else:
+        # parse in graph from given .gml file
         if args[1] == "--input":
             input_file = args[2]
             # TODO: call file parser from file_io: user_graph = fio.parse_graph(input_file)
             iterator += 2
+        # generate graph with given n and c
         elif end > 3 and args[1] == "--create_random_graph":
             n = args[2]
             c = args[3]
-            # TODO: call graph generator from generator: user_graph = gen.generate(n, c) <- check that there is error handling for n/c type
+            # TODO: call graph generator from generator: user_graph = gen.generate(n, c)
             iterator += 3
+        # if there are 3 arguments and we aren't inputting a file, then not enough arguments to generate graph. terminate program.
         else:
             raise Exception("Program was terminated because it was missing '--create_random_graph arguments'. Requires 'n' (number of nodes) and 'c' (probability of an edge forming).")
 
+        # make a list of all BFS nodes given and call the BFS function
         if iterator < end and args[iterator] == "--multi_BFS":
             remaining_args = args[iterator + 1:]
 
             for i in range(len(remaining_args)):
+                # check if the current argument is the next command and not another node. if it's the next command, stop iterating.
                 if "--" not in remaining_args[i]:
                     multi_BFS_nodes.append(remaining_args[i])
                 else:
@@ -37,15 +44,19 @@ def main():
             
             # TODO: call BFS from analysis: result = BFS(user_graph, multi_BFS_nodes)
         
+        # call the analysis function
         if iterator < end and args[iterator] == "--analyze":
             # TODO: call analysis from analysis: result = analysis(user_graph)
             iterator += 1
         
+        # call the visualization function
         if iterator < end and args[iterator] == "--plot":
             # TODO: call plot from visualization: result = visualization(user_graph)
             iterator += 1
         
+        # call the output function
         if iterator < end and args[iterator] == "--output":
+            # check if the output file name is missing. if so, terminate program.
             if iterator + 1 == end:
                 raise Exception("Program was terminated because it was missing the output file name.")
             
@@ -53,6 +64,7 @@ def main():
             iterator += 2
             # TODO: call file output from file_io: fio.save_graph(user_graph, output_file)
         
+        # check to see if there are extra arguments provided. if so, print them so the user can see.
         if iterator < end:
             print("NOTE: extra arguments at the end of the input string were ignored:", args[iterator:])
 
