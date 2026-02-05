44632ee7235c203ab5557056192b76287ee0900f
gracekflores <168140240+gracekflores@users.noreply.github.com>
2026-02-04 21:10:05 -0800

    commented out testing block
    It's not going to be part of the final version of the file, but it's still good for testing.

--- plot.py
+++ plot.py
@@ -105,111 +105,111 @@ def plot(graph, isolated_nodes, highlight_edges):
     net.show("graph.html", notebook=False)
 
 
-###### Testing #######
-def make_test_graph():
-    G = nx.Graph()
+# ###### Testing #######
+# def make_test_graph():
+#     G = nx.Graph()
 
-    # Component 1: A triangle (cycle)
-    G.add_edges_from([(1, 2), (2, 3), (3, 1)])
+#     # Component 1: A triangle (cycle)
+#     G.add_edges_from([(1, 2), (2, 3), (3, 1)])
 
-    # Component 2: A simple chain
-    G.add_edges_from([(4, 5), (5, 6)])
+#     # Component 2: A simple chain
+#     G.add_edges_from([(4, 5), (5, 6)])
 
-    # Isolated node
-    G.add_node(7)
+#     # Isolated node
+#     G.add_node(7)
 
-    return G
+#     return G
 
-def analyze(graph):
-    """
-    Analyze the structural properties of a NetworkX graph and compute
-    data for visualization.
+# def analyze(graph):
+#     """
+#     Analyze the structural properties of a NetworkX graph and compute
+#     data for visualization.
 
-    Args:
-        graph (networkx.Graph) : the input graph to analyze
+#     Args:
+#         graph (networkx.Graph) : the input graph to analyze
 
-    Returns:
-        dict: 
-            A dictionary containing:
-            - 'graph' (networkx.Graph) : the original graph
-            - 'components' (list of sets) : each set contains the nodes in one connected component
-            - 'isolated_nodes' (list) : a list of nodes with no incident edges
-            - 'highlight_edges' (set of tuples) : a set of edges (u, v) that appear in at least 
-                one shortest path computed via breadth-first search / Dijkstra
-    """
-    
-    # Loading the graph
-    #G = nx.read_gml(graph)
-    G = graph
+#     Returns:
+#         dict: 
+#             A dictionary containing:
+#             - 'graph' (networkx.Graph) : the original graph
+#             - 'components' (list of sets) : each set contains the nodes in one connected component
+#             - 'isolated_nodes' (list) : a list of nodes with no incident edges
+#             - 'highlight_edges' (set of tuples) : a set of edges (u, v) that appear in at least 
+#                 one shortest path computed via breadth-first search / Dijkstra
+#     """
 
-    # Looking for connected components
-    components = list(nx.connected_components(G))
-    num_components = len(components)
-    print("Connected components:", num_components)
+#     # Loading the graph
+#     #G = nx.read_gml(graph)
+#     G = graph
 
-    # Detecting cycles
-    has_cycle = 0
-    if (nx.is_directed(G)):
-        has_cycle = not nx.is_directed_acyclic_graph(G)
-    else:
-        has_cycle = len(nx.cycle_basis(G)) > 0
+#     # Looking for connected components
+#     components = list(nx.connected_components(G))
+#     num_components = len(components)
+#     print("Connected components:", num_components)
 
-    result = 0
-    if has_cycle:
-        result = "Yes"
-    else:
-        result = "No"
+#     # Detecting cycles
+#     has_cycle = 0
+#     if (nx.is_directed(G)):
+#         has_cycle = not nx.is_directed_acyclic_graph(G)
+#     else:
+#         has_cycle = len(nx.cycle_basis(G)) > 0
 
-    print("Contains cycle?:", result)
+#     result = 0
+#     if has_cycle:
+#         result = "Yes"
+#     else:
+#         result = "No"
 
-    # Detecting isolated nodes
-    isolated_nodes = list(nx.isolates(G))
-    print("Isolated nodes:", isolated_nodes)
+#     print("Contains cycle?:", result)
 
-    # Computing density
-    density = nx.density(G)
-    print("Graph density:", density)
+#     # Detecting isolated nodes
+#     isolated_nodes = list(nx.isolates(G))
+#     print("Isolated nodes:", isolated_nodes)
 
-    # Computing average shortest path length
-    if nx.is_connected(G):
-        avg_path_len = nx.average_shortest_path_length(G)
-        print("Average shortest path length:", avg_path_len)
-    else:
-        print("Graph is not connected; cannot compute average shortest path length.")
+#     # Computing density
+#     density = nx.density(G)
+#     print("Graph density:", density)
 
-    ## Data for plotting ##
-    # Computing BFS shortest paths from each root
-    bfs_roots = [next(iter(c)) for c in components]
+#     # Computing average shortest path length
+#     if nx.is_connected(G):
+#         avg_path_len = nx.average_shortest_path_length(G)
+#         print("Average shortest path length:", avg_path_len)
+#     else:
+#         print("Graph is not connected; cannot compute average shortest path length.")
 
-    shortest_paths = []
-    for root in bfs_roots:
-        lengths, paths = nx.single_source_dijkstra(G, root)  # works for weighted or unweighted
-        shortest_paths.append(paths)
+#     ## Data for plotting ##
+#     # Computing BFS shortest paths from each root
+#     bfs_roots = [next(iter(c)) for c in components]
 
-    # Marking edges that belong to any shortest path
-    highlight_edges = set()
-    for paths in shortest_paths:
-        for target, path in paths.items():
-            if len(path) > 1:
-                edges = list(zip(path[:-1], path[1:]))
-                highlight_edges.update(edges)
+#     shortest_paths = []
+#     for root in bfs_roots:
+#         lengths, paths = nx.single_source_dijkstra(G, root)  # works for weighted or unweighted
+#         shortest_paths.append(paths)
 
-
-    return {
-        "graph": G,
-        "components": components,            # list of sets
-        "isolated_nodes": isolated_nodes,    # list
-        "highlight_edges": highlight_edges,  # set of tuples
-    }
+#     # Marking edges that belong to any shortest path
+#     highlight_edges = set()
+#     for paths in shortest_paths:
+#         for target, path in paths.items():
+#             if len(path) > 1:
+#                 edges = list(zip(path[:-1], path[1:]))
+#                 highlight_edges.update(edges)
 
 
-test_graph = make_test_graph()
-results = analyze(test_graph)
+#     return {
+#         "graph": G,
+#         "components": components,            # list of sets
+#         "isolated_nodes": isolated_nodes,    # list
+#         "highlight_edges": highlight_edges,  # set of tuples
+#     }
 
-# nx.write_gml(results["graph"], "graph.gml")     to export to .gml
 
-plot(
-    results["graph"],
-    results["isolated_nodes"],
-    results["highlight_edges"]
-)
+# test_graph = make_test_graph()
+# results = analyze(test_graph)
+
+# # nx.write_gml(results["graph"], "graph.gml")     to export to .gml
+
+# plot(
+#     results["graph"],
+#     results["isolated_nodes"],
+#     results["highlight_edges"]
+# )
